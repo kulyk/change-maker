fce328d4e68eb1092456cfcf01fae1a9133140df
Anton Kulyk <kuliks.anton@gmail.com>
2021-02-23 14:06:39 +0200

    Fix wrong intermediate variants in the results

--- index.test.js
+++ index.test.js
@@ -1,69 +1,90 @@
 const { makeChange } = require("./makeChange");
 
 describe("makeChange", () => {
-	it("calculates change for single number", () => {
-		expect(makeChange(7, [1])).toEqual(expect.arrayContaining([{ 1: 7 }]));
-	});
+  it("calculates change for single number", () => {
+    expect(makeChange(7, [1])).toEqual(expect.arrayContaining([{ 1: 7 }]));
+  });
 
-	it("calculates change for a few numbers", () => {
-		expect(makeChange(7, [2, 5])).toEqual(
-			expect.arrayContaining([{ 2: 1, 5: 1 }])
-		);
-	});
+  it("calculates change for a few numbers", () => {
+    expect(makeChange(7, [2, 5])).toEqual(
+      expect.arrayContaining([{ 2: 1, 5: 1 }])
+    );
+  });
 
-	it("calculates change if coins have to be summed >1 times", () => {
-		expect(makeChange(30, [4, 9])).toEqual(
-			expect.arrayContaining([{4: 3, 9: 2}])
-		)
-	})
+  it("calculates change if coins have to be summed >1 times", () => {
+    expect(makeChange(30, [4, 9])).toEqual(
+      expect.arrayContaining([{ 4: 3, 9: 2 }])
+    );
+  });
 
-	it("combines different variants", () => {
-		expect(makeChange(7, [1, 5])).toEqual(
-			expect.arrayContaining([{ 1: 7 }, { 1: 2, 5: 1 }])
-		);
-	});
 
-	it("throws an error if coinset is empty", () => {
-		expect(() => makeChange(7, [])).toThrow();
-	});
+  it("calculates change for a lot of numbers", () => {
+    expect(makeChange(19, [1, 2, 4, 8])).toEqual(
+      expect.arrayContaining([
+        { 1: 19 },
+        { 1: 17, 2: 1 },
+        { 1: 15, 4: 1 },
+        { 1: 11, 8: 1 },
+        // { 1: 1, 2: 3, 4: 1, 8: 1 },
+      ])
+    );
+  });
 
-	it("throws an error if change cant be calculated", () => {
-		expect(() => makeChange(7, [3, 8])).toThrow();
-	});
+  it("does not include results to be intermediately dropped", () => {
+    expect(makeChange(19, [1, 4, 8])).not.toEqual(
+      expect.arrayContaining([
+        {4: 1, 8: 1},
+      ])
+    );
+  });
+
+  it("combines different variants", () => {
+    expect(makeChange(7, [1, 5])).toEqual(
+      expect.arrayContaining([{ 1: 7 }, { 1: 2, 5: 1 }])
+    );
+  });
+
+  it("throws an error if coinset is empty", () => {
+    expect(() => makeChange(7, [])).toThrow();
+  });
+
+  it("throws an error if change cant be calculated", () => {
+    expect(() => makeChange(7, [3, 8])).toThrow();
+  });
 });
 
 const testCases = [
-	{
-		x: 6,
-		coinset: [1, 5, 10, 25],
-		expected: [{ 1: 6 }, { 1: 1, 5: 1 }],
-	},
-	{
-		x: 6,
-		coinset: [3, 4],
-		expected: [{ 3: 2 }],
-	},
-	{
-		x: 6,
-		coinset: [1, 3, 4],
-		expected: [{ 1: 6 }, { 3: 2 }, { 1: 3, 3: 1 }, { 1: 2, 4: 1 }],
-	},
-	{
-		x: 6,
-		coinset: [5, 7],
-		expected: "error",
-	},
+  {
+    x: 6,
+    coinset: [1, 5, 10, 25],
+    expected: [{ 1: 6 }, { 1: 1, 5: 1 }],
+  },
+  {
+    x: 6,
+    coinset: [3, 4],
+    expected: [{ 3: 2 }],
+  },
+  {
+    x: 6,
+    coinset: [1, 3, 4],
+    expected: [{ 1: 6 }, { 3: 2 }, { 1: 3, 3: 1 }, { 1: 2, 4: 1 }],
+  },
+  {
+    x: 6,
+    coinset: [5, 7],
+    expected: "error",
+  },
 ];
 
 testCases.forEach((testCase) => {
-	const { x, coinset, expected } = testCase;
-	const expectedStr = JSON.stringify(expected);
-	const coinsetStr = coinset.join(", ");
-	test(`Returns ${expectedStr} if x is ${x} for ${coinsetStr}`, () => {
-		if (expected === "error") {
-			expect(() => makeChange(x, coinset)).toThrow();
-		} else {
-			expect(makeChange(x, coinset)).toEqual(expect.arrayContaining(expected));
-		}
-	});
+  const { x, coinset, expected } = testCase;
+  const expectedStr = JSON.stringify(expected);
+  const coinsetStr = coinset.join(", ");
+  test(`Returns ${expectedStr} if x is ${x} for ${coinsetStr}`, () => {
+    if (expected === "error") {
+      expect(() => makeChange(x, coinset)).toThrow();
+    } else {
+      expect(makeChange(x, coinset)).toEqual(expect.arrayContaining(expected));
+    }
+  });
 });
--- makeChange.js
+++ makeChange.js
@@ -1,58 +1,60 @@
-function checkTwoCoins(x, coin1, coin2, result={[coin1]: 0, [coin2]: 0}) {
-	const sum = coin1 + coin2;
-	if (sum > x) {
-		return result;
-	}
+function checkTwoCoins(x, coin1, coin2, result = { [coin1]: 0, [coin2]: 0 }) {
+  const sum = coin1 + coin2;
+  if (sum > x) {
+    return null;
+  }
 
-	result[coin1]++
-	result[coin2]++
+  result[coin1]++;
+  result[coin2]++;
 
-	if (sum === x) {
-		return result;
-	}
+  if (sum === x) {
+    return result;
+  }
 
-	const diff = x - sum;
-	const canSumWithCoin1 = diff % coin1 === 0;
-	const canSumWithCoin2 = diff % coin2 === 0;
-	if (!canSumWithCoin1 && !canSumWithCoin2) {
-		if (diff > coin1 || diff > coin2) {
-			return checkTwoCoins(diff, coin1, coin2, result)
-		}
-		return null;
-	}
-	if (canSumWithCoin1) {
-		result[coin1] += diff / coin1
-	} else {
-		result[coin2] += diff / coin1
-	}
-	return result
+  const diff = x - sum;
+  const canSumWithCoin1 = diff % coin1 === 0;
+  const canSumWithCoin2 = diff % coin2 === 0;
+  if (!canSumWithCoin1 && !canSumWithCoin2) {
+    if (diff > coin1 || diff > coin2) {
+      return checkTwoCoins(diff, coin1, coin2, result);
+    }
+    return null;
+  }
+
+  if (canSumWithCoin1) {
+    result[coin1] += diff / coin1;
+  } else {
+    result[coin2] += diff / coin1;
+  }
+
+  return result;
 }
 
 function makeChange(x, initialCoinSet) {
-	const result = [];
+  const result = [];
 
-	// Coins that are already bigger than x are useless here
-	const coinSet = initialCoinSet.filter((coin) => coin < x);
+  // Coins that are already bigger than x are useless here
+  const coinSet = initialCoinSet.filter((coin) => coin < x);
 
-	coinSet.forEach((coin, i) => {
-		if (x % coin === 0) {
-			result.push({ [coin]: x / coin });
-		}
-		for (let nextIndex = i + 1; nextIndex < coinSet.length; nextIndex++) {
-			const nextCoin = coinSet[nextIndex];
-			const sums = checkTwoCoins(x, coin, nextCoin);
-			if (sums) {
-				result.push(sums);
-			}
-		}
-	});
+  coinSet.forEach((coin, i) => {
+    if (x % coin === 0) {
+      result.push({ [coin]: x / coin });
+    }
+    for (let nextIndex = i + 1; nextIndex < coinSet.length; nextIndex++) {
+      const nextCoin = coinSet[nextIndex];
+      const sums = checkTwoCoins(x, coin, nextCoin);
+      if (sums) {
+        result.push(sums);
+      }
+    }
+  });
 
-	if (result.length) {
-		return result;
-	}
-	throw new Error("X cannot be calculated from given coins");
+  if (result.length) {
+    return result;
+  }
+  throw new Error("X cannot be calculated from given coins");
 }
 
 module.exports = {
-	makeChange,
+  makeChange,
 };
